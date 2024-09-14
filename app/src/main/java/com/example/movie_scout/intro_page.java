e2b491426a3e60acad796fc576a20d9466481247
Gen <lirazangm@gmail.com>
2024-09-14 20:02:25 +0800

    MovieScout

--- app/src/main/java/com/example/movie_scout/intro_page.java
+++ app/src/main/java/com/example/movie_scout/intro_page.java
@@ -1,6 +1,10 @@
 package com.example.movie_scout;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,16 @@ public class intro_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_intro_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btn1 = findViewById(R.id.btn_get);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(intro_page.this,MainActivity.class));
+            }
         });
+
+
+
     }
 }
